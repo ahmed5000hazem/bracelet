e62e276a26291b999d18711334d8097d7353971b
ahmed hazem <ahmed5000hazem@gmail.com>
2023-03-03 17:58:18 +0200

    user create

---
+++ routes/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Users\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::middleware(['auth'])->prefix('users')->group(function () {
+    Route::get('create', [UserController::class, 'create'])->name('users-create');
+
+});
