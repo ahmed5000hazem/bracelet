adb578116be10454d7d96cbe0bec9fbc49310ef0
ahmed hazem <ahmed5000hazem@gmail.com>
2023-03-03 17:49:50 +0200

    update product migration

--- database/migrations/2023_02_28_212623_create_products_table.php
+++ database/migrations/2023_02_28_212623_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->text('description')->nullable();
-            $table->jsonb('custom')->default('"[]"');
+            $table->jsonb('custom')->nullable();
             $table->integer('qty');
             $table->double('price');
             $table->double('discount');
